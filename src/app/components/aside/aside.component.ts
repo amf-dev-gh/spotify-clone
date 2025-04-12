5b78ae6d15485b4e0b9e3bd801f6c730bbf383bb
Andres Fernandez <andres94.dev@gmail.com>
2025-04-12 20:05:48 +0200

    implementacion del aside

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { IconComponent } from "../icon/icon.component";
+import { PlaylistCardComponent } from "./playlist-card/playlist-card.component";
+import { Playlist, PLAYLISTS } from '../../consts/data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-aside',
-  imports: [],
+  imports: [IconComponent, PlaylistCardComponent, CommonModule],
   templateUrl: './aside.component.html',
   styleUrl: './aside.component.css'
 })
 export class AsideComponent {
+
+  playLists:Playlist[] = PLAYLISTS;
 
 }
