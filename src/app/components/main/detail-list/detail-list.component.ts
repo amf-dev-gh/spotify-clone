145b63ba4cebf38502ae06c26c96a1a5e04ba29b
Andres Fernandez <andres94.dev@gmail.com>
2025-04-13 19:05:53 +0200

    cargando listado de canciones en vista de lista

--- src/app/components/main/detail-list/detail-list.component.ts
+++ src/app/components/main/detail-list/detail-list.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Playlist } from '../../../consts/interfaces';
-import { ALL_PLAYLIST } from '../../../consts/data';
+import { Playlist, Song } from '../../../consts/interfaces';
+import { ALL_PLAYLIST, SONGS } from '../../../consts/data';
 import { IconComponent } from "../../icon/icon.component";
 import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail-list',
-  imports: [IconComponent],
+  imports: [IconComponent, CommonModule],
   templateUrl: './detail-list.component.html'
 })
 export class DetailListComponent implements OnInit {
 
   playlist?:Playlist;
+  playListSongs?:Song[];
 
   constructor(private route: ActivatedRoute, private router:Router) { }
 
@@ -27,9 +29,14 @@ export class DetailListComponent implements OnInit {
     const playlist = ALL_PLAYLIST.find(pl => pl.id === id);
     if(playlist){
       this.playlist = playlist;
+      this.getSongs(playlist.albumId);
     }else{
       console.error("Playlist not found")
       this.router.navigate(['/home']);
     }
+  }
+
+  getSongs(albumId:number){
+    this.playListSongs = SONGS.filter(s => s.albumId === albumId);
   }
 }
