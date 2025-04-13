7d83837224e30ae61eb99ec4a65dfa40e56eb92a
Andres Fernandez <andres94.dev@gmail.com>
2025-04-13 20:07:15 +0200

    implementada funcionalidad para reproducir una cancion desde el detalle de la lista

--- src/app/components/main/detail-list/detail-list.component.ts
+++ src/app/components/main/detail-list/detail-list.component.ts
@@ -4,6 +4,7 @@ import { ALL_PLAYLIST, SONGS } from '../../../consts/data';
 import { IconComponent } from "../../icon/icon.component";
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { SongService } from '../../../services/song.service';
 
 @Component({
   selector: 'app-detail-list',
@@ -12,31 +13,34 @@ import { CommonModule } from '@angular/common';
 })
 export class DetailListComponent implements OnInit {
 
-  playlist?:Playlist;
-  playListSongs?:Song[];
+  playlist?: Playlist;
+  playListSongs?: Song[];
 
-  constructor(private route: ActivatedRoute, private router:Router) { }
+  constructor(private route: ActivatedRoute, private router: Router, private songService:SongService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       const id = params['id'];
-      console.log('Id de la lista:', id);
       this.getPlayList(id);
     });
   }
 
-  getPlayList(id:string){
+  getPlayList(id: string) {
     const playlist = ALL_PLAYLIST.find(pl => pl.id === id);
-    if(playlist){
+    if (playlist) {
       this.playlist = playlist;
       this.getSongs(playlist.albumId);
-    }else{
+    } else {
       console.error("Playlist not found")
       this.router.navigate(['/home']);
     }
   }
 
-  getSongs(albumId:number){
+  getSongs(albumId: number) {
     this.playListSongs = SONGS.filter(s => s.albumId === albumId);
+  }
+
+  selectSong(song:Song){
+    this.songService.setSong(song);
   }
 }
