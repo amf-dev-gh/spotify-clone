44ce5fa5b1dc9a510416d1059da821df25faf0a5
Andres Fernandez <andres94.dev@gmail.com>
2025-04-13 21:04:45 +0200

    implementado servicio para cambiar siguiente o anterior cancion

--- src/app/services/song.service.ts
+++ src/app/services/song.service.ts
@@ -14,4 +14,34 @@ export class SongService {
     this.$currentSong.set(song);
   }
 
+  nextSong() {
+    const index = this.currentIndex();
+    if (index != -1) {
+      const nextSong = SONGS[index + 1];
+      if (nextSong) {
+        this.setSong(nextSong);
+      } else {
+        this.setSong(SONGS[0]);
+      }
+    }
+  }
+
+  prevSong() {
+    const index = this.currentIndex();
+    if (index != -1) {
+      const prevSong = SONGS[index - 1];
+      if (prevSong) {
+        this.setSong(prevSong);
+      } else {
+        this.setSong(SONGS[SONGS.length-1]);
+      }
+    }
+  }
+
+  private currentIndex(): number {
+    const currentSongId = this.$currentSong().id;
+    const currentAlbumId = this.$currentSong().albumId
+    return SONGS.findIndex(song => song.id === currentSongId && song.albumId === currentAlbumId);
+  }
+
 }
