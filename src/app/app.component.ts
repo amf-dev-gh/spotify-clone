c080000b3d180656e3b68a2ecd32affb0e954776
Andres Fernandez <andres94.dev@gmail.com>
2025-04-14 15:14:21 +0200

    añado verificacion de movil para mostrar el modo correspondiente al iniciar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./components/nav/nav.component";
 import { AsideComponent } from "./components/aside/aside.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { CommonModule } from '@angular/common';
+import { MovileHomeComponent } from "./movile-compponents/movile-home/movile-home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, AsideComponent, FooterComponent],
+  imports: [RouterOutlet, NavComponent, AsideComponent, FooterComponent, CommonModule, MovileHomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  isMobile = false;
+
+  ngOnInit(): void {
+    this.checkIfMobile();
+  }
+
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkIfMobile();
+  }
+
+  checkIfMobile() {
+    this.isMobile = window.innerWidth <= 768;
+  }
 
 }
