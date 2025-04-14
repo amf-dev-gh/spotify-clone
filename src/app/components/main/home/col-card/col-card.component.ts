c03c17810f5d23b429855b9be4b5c96a0971a1c7
Andres Fernandez <andres94.dev@gmail.com>
2025-04-14 13:27:53 +0200

    implementada funcionalidad para dar play desde row card o col card de la home

--- src/app/components/main/home/col-card/col-card.component.ts
+++ src/app/components/main/home/col-card/col-card.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { Playlist } from '../../../../consts/interfaces';
+import { Component, inject, Input } from '@angular/core';
+import { Playlist, Song } from '../../../../consts/interfaces';
 import { IconComponent } from "../../../icon/icon.component";
 import { RouterLink } from '@angular/router';
+import { SongService } from '../../../../services/song.service';
+import { SONGS } from '../../../../consts/data';
 
 @Component({
   selector: 'app-col-card',
@@ -12,5 +14,27 @@ import { RouterLink } from '@angular/router';
 export class ColCardComponent {
 
   @Input()playList?:Playlist;
+  private readonly songService = inject(SongService);
+  readonly currentPlaylistName = this.songService.$currentPlayListName;
+  readonly currentSong = this.songService.$currentSong;
+  playListSongs: Song[] = [];
+
+  startPlayList() {
+    if (this.playList) {
+      this.getSongs(this.playList.albumId);
+      const song = this.playListSongs[0];
+      if (song) {
+        if (song === this.currentSong()) {
+          this.songService.setPlay()
+          return;
+        }
+        this.songService.setSong(song, this.playList?.title);
+      }
+    }
+  }
+
+  getSongs(albumId: number) {
+    this.playListSongs = SONGS.filter(s => s.albumId === albumId);
+  }
 
 }
--- src/app/components/main/home/row-card/row-card.component.ts
+++ src/app/components/main/home/row-card/row-card.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core';
-import { Playlist } from '../../../../consts/interfaces';
+import { Component, inject, Input } from '@angular/core';
+import { Playlist, Song } from '../../../../consts/interfaces';
 import { IconComponent } from "../../../icon/icon.component";
 import { RouterLink } from '@angular/router';
+import { SongService } from '../../../../services/song.service';
+import { SONGS } from '../../../../consts/data';
 
 @Component({
   selector: 'app-row-card',
@@ -11,5 +13,27 @@ import { RouterLink } from '@angular/router';
 export class RowCardComponent {
 
   @Input()playList?:Playlist;
+  private readonly songService = inject(SongService);
+  readonly currentPlaylistName = this.songService.$currentPlayListName;
+  readonly currentSong = this.songService.$currentSong;
+  playListSongs: Song[] = [];
+
+  startPlayList() {
+    if (this.playList) {
+      this.getSongs(this.playList.albumId);
+      const song = this.playListSongs[0];
+      if (song) {
+        if (song === this.currentSong()) {
+          this.songService.setPlay()
+          return;
+        }
+        this.songService.setSong(song, this.playList?.title);
+      }
+    }
+  }
+
+  getSongs(albumId: number) {
+    this.playListSongs = SONGS.filter(s => s.albumId === albumId);
+  }
 
 }
